464d98e88f9a051473aa07211a23f3c0587371c3
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-25 03:32:07 -0700

    updated currency system
    also added in reload scene if player runs out of money

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] GameObject towerPrefab;
-
+    [SerializeField] Tower towerPrefab;
+    
     [SerializeField] bool isPlaceable;
     public bool IsPlaceable { get { return isPlaceable; } }
 
-    void OnMouseDown() 
+
+    void OnMouseDown()
     {
-        if (isPlaceable)
+        if(isPlaceable)
         {
-            Instantiate(towerPrefab, transform.position, Quaternion.identity);
-            isPlaceable = false;
+            bool isPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceable = !isPlaced;
         }
     }
 }
+
