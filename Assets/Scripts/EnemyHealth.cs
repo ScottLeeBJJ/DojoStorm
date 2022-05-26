7f6c0b8b96158901b2b2833bb53d6cb6da1f4818
ScottLeeBJJ <scottlee5455@outlook.com>
2022-05-25 19:57:10 -0700

    minor update

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
-    [SerializeField] int currentHitPoints = 0;
 
+    [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
+    [SerializeField] int difficultyRamp = 1;
+
+    int currentHitPoints = 0;
     Enemy enemy;
+
 
     void OnEnable()
     {
-            currentHitPoints = maxHitPoints;
+        currentHitPoints = maxHitPoints;        
     }
 
-    void Start() 
+    void Start()
     {
         enemy = GetComponent<Enemy>();
     }
@@ -24,14 +29,16 @@ public class EnemyHealth : MonoBehaviour
         ProcessHit();
     }
 
-    void ProcessHit()
+    void ProcessHit() 
     {
-        currentHitPoints --;
+        currentHitPoints--;
 
-        if (currentHitPoints <= 0)
+        if(currentHitPoints <= 0)
         {
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
             enemy.RewardGold();
         }
     }
 }
+
